da57c8dbef97e9ee8746d9c5a3f70ffd0898bd9f
MichealWayne <michealwayne@163.com>
2023-04-18 11:37:32 +0000

    CI updated the documentation

---
+++ docs/v1.0.0-beta01/frontend/webComponents/Member/index.d.ts
@@ -0,0 +1,7 @@
+export * from './YafMember.js';
+export * from './YafMemberDeclaration.js';
+export * from './YafMemberGetterSetter.js';
+export * from './YafMemberGroupLink.js';
+export * from './YafMemberGroupReflection.js';
+export * from './YafMemberSignatures.js';
+export * from './YafMemberSources.js';
