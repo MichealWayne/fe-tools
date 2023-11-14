0254a504cb585db906bd4f4169833c1b14accc9f
Wayne <michealwayne@163.com>
2023-11-14 10:26:03 +0800

    fix ai-utils export document

---
+++ utils/packages/ai-utils/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Wayne
+ * @Date 2023-09-05 09:38:19
+ * @LastEditTime 2023-11-11 10:17:13
+ */
+// gpt application
+export * from './applications/codeReview';
+export * from './applications/sql';
+
+// llm utils
+export * from './llm/prompts';
+
+// basic utils
+export * from './utils/python';
