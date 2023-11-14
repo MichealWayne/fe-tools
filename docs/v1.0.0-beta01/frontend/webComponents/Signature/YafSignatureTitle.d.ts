c8c0e5526a6d1744d63406c6633649487b05ebd4
MichealWayne <michealwayne@163.com>
2023-11-14 05:15:59 +0000

    CI updated the documentation

---
+++ docs/v1.0.0-beta01/frontend/webComponents/Signature/YafSignatureTitle.d.ts
@@ -0,0 +1,6 @@
+import { yafSignatureTitleProps } from '../../../types/frontendTypes.js';
+import { YafHTMLElement } from '../../index.js';
+export declare class YafSignatureTitle extends YafHTMLElement<yafSignatureTitleProps> {
+    onConnect(): void;
+    private static factory;
+}
