8ff6a9c95c34be9453d785e66c9e8dd6ffe2bd6f
Wayne <michealwayne@163.com>
2024-04-30 13:43:29 +0800

    feat update web-utils

--- docs/v1.0.0-beta01/frontend/webComponents/Signature/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './YafSignature.js';
-export * from './YafSignatureBody.js';
-export * from './YafSignatureParameters.js';
-export * from './YafSignatureParametersType.js';
-export * from './YafSignatureTitle.js';
-export * as signatureTypes from './signatureTypes/index.js';
