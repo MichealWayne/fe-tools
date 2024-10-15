b6dbbad03b15a39564a3cb4de408c0e2f41c2218
Wayne <michealwayne@163.com>
2024-10-15 13:50:37 +0800

    feat update web utils

--- utils/packages/web-utils/src/index.ts
+++ utils/packages/web-utils/src/index.ts
@@ -1,11 +1,12 @@
 /**
  * @author Wayne
  * @Date 2022-07-05 13:34:44
- * @LastEditTime 2024-07-07 13:58:25
+ * @LastEditTime 2024-10-13 13:41:19
  */
 
 export * from './cookies';
 export * from './css';
+export * from './clipboard';
 export * from './dom';
 export * from './file';
 export * from './image';
@@ -16,3 +17,5 @@ export * from './rem';
 export * from './screen';
 export * from './storage';
 export * from './url';
+
+export * from './others';
