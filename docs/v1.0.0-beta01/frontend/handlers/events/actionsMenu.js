933f39a242f29d5c187126410144fc04ccd9032d
Wayne <michealwayne@163.com>
2025-04-01 13:12:31 +0800

    feat update chrome-extension

--- docs/v1.0.0-beta01/frontend/handlers/events/actionsMenu.js
+++
@@ -1,8 +0,0 @@
-import { trigger } from './triggers.js';
-export const rollMenuDown = () => new Event(trigger.menu.rollMenuDown);
-export const rollMenuUp = () => new Event(trigger.menu.rollMenuUp);
-export const toggle = (state) => new CustomEvent(trigger.menu.toggle, {
-    detail: { state },
-});
-export const search = (searchString) => new CustomEvent(trigger.menu.search, { detail: { searchString } });
-//# sourceMappingURL=actionsMenu.js.map
