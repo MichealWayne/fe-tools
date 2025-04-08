879917a6b86ade229df00845d50d8b47e0d3ff8a
Wayne <michealwayne@163.com>
2025-04-08 11:49:46 +0800

    feat update ai-utils

--- docs/v1.0.0-beta01/frontend/webComponents/Member/YafMemberSources.d.ts
+++
@@ -1,5 +0,0 @@
-import { YafDeclarationReflection, YafSignatureReflection } from '../../../types/types.js';
-import { YafHTMLElement } from '../../index.js';
-export declare class YafMemberSources extends YafHTMLElement<YafSignatureReflection | YafDeclarationReflection> {
-    onConnect(): void;
-}
