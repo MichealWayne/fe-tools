d4637c917eff40daeddf7fd8f320bb7ced56a311
Wayne <michealwayne@163.com>
2026-02-03 20:05:48 +0800

    feat update links

--- docs/v1.0.0-beta01/frontend/webComponents/Content/YafContentMarked.d.ts
+++
@@ -1,5 +0,0 @@
-import { htmlString } from '../../../types/types.js';
-import { YafHTMLElement } from '../../index.js';
-export declare class YafContentMarked extends YafHTMLElement<htmlString | undefined> {
-    onConnect(): void;
-}
--- docs/v1.0.0-beta01/frontend/webComponents/Navigation/YafNavigationLink.d.ts
+++
@@ -1,6 +0,0 @@
-import { YafHTMLElement } from '../../index.js';
-export declare class YafNavigationLink extends YafHTMLElement {
-    aHTMLElement: HTMLAnchorElement;
-    onConnect(): void;
-    disconnectedCallback(): void;
-}
