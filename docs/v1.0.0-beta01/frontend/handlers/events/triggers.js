6c65175dc0eeeed63e64a204377ce8a9d29e4fbc
MichealWayne <michealwayne@163.com>
2026-02-03 12:09:13 +0000

    CI updated the documentation

---
+++ docs/v1.0.0-beta01/frontend/handlers/events/triggers.js
@@ -0,0 +1,23 @@
+export const trigger = {
+    content: {
+        setLocation: 'yaf.content.setLocation',
+        scrollTo: 'yaf.content.scrollTo',
+        scrollTop: 'yaf.content.scrollTop',
+        getPageId: 'yaf.content.getPageId',
+        breadcrumb: 'yaf.content.breadcrumb',
+    },
+    menu: {
+        rollMenuDown: 'yaf.menu.rollMenuDown',
+        rollMenuUp: 'yaf.menu.rollMenuUp',
+        scrollTo: 'yaf.menu.scrollTo',
+        toggle: 'yaf.menu.toggle',
+        search: 'yaf.menu.search',
+    },
+    drawers: {
+        resetHeight: 'yaf.drawer.resetHeight',
+    },
+    options: {
+        display: 'yaf.options.display',
+    },
+};
+//# sourceMappingURL=triggers.js.map
