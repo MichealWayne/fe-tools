3f93241ea3ba9c55848449ba271baa8b90525601
Wayne <michealwayne@163.com>
2026-03-17 20:37:04 +0800

    feat update skills & utils

--- docs/v1.0.0-beta01/frontend/webComponents/Signature/signatureTypes/YafSignatureConditional.d.ts
+++
@@ -1,5 +0,0 @@
-import { JSONOutput } from 'typedoc';
-import { YafHTMLElement } from '../../../index.js';
-export declare class YafSignatureConditional extends YafHTMLElement<JSONOutput.ConditionalType> {
-    onConnect(): void;
-}
