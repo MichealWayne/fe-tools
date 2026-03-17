86a4347d3d5b4443d1b6ec9da52177570c471bd1
Wayne <michealwayne@163.com>
2026-03-17 20:37:04 +0800

    feat update skills & utils

--- docs/v1.0.0-beta01/frontend/handlers/events/actionsOptions.js
+++
@@ -1,5 +0,0 @@
-import { trigger } from './triggers.js';
-export const display = (key, value) => new CustomEvent(trigger.options.display, {
-    detail: { key, value },
-});
-//# sourceMappingURL=actionsOptions.js.map
--- docs/v1.0.0-beta01/frontend/webComponents/Member/YafMemberGetterSetter.d.ts
+++
@@ -1,6 +0,0 @@
-import { YAFDataObject } from '../../../types/types.js';
-import { YafHTMLElement } from '../../index.js';
-export declare class YafMemberGetterSetter extends YafHTMLElement<YAFDataObject> {
-    onConnect(): void;
-    private static factory;
-}
