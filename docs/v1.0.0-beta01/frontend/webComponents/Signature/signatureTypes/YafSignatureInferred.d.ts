140f664bdc6cee2925c61e0c3fcf44c9d991c8c2
Wayne <michealwayne@163.com>
2026-05-12 13:10:45 +0000

    CI updated the documentation

---
+++ docs/v1.0.0-beta01/frontend/webComponents/Signature/signatureTypes/YafSignatureInferred.d.ts
@@ -0,0 +1,5 @@
+import { JSONOutput } from 'typedoc';
+import { YafHTMLElement } from '../../../index.js';
+export declare class YafSignatureInferred extends YafHTMLElement<JSONOutput.InferredType> {
+    onConnect(): void;
+}
---
+++ docs/v1.0.0-beta01/frontend/webComponents/Signature/signatureTypes/YafSignatureIntrinsic.d.ts
@@ -0,0 +1,5 @@
+import { JSONOutput } from 'typedoc';
+import { YafHTMLElement } from '../../../index.js';
+export declare class YafContentSignatureIntrinsic extends YafHTMLElement<JSONOutput.IntrinsicType> {
+    onConnect(): void;
+}
