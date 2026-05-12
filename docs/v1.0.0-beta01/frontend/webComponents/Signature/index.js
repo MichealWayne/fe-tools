fe1c9dffc10bbe6f6c9dd3f9de9d22a79e9bf44f
Wayne <michealwayne@163.com>
2026-05-12 21:03:15 +0800

    feat update (be)templates&utils md

--- docs/v1.0.0-beta01/frontend/webComponents/Signature/index.js
+++
@@ -1,8 +0,0 @@
-export * from './YafSignature.js';
-export * from './YafSignatureBody.js';
-export * from './YafSignatureParameters.js';
-export * from './YafSignatureParametersType.js';
-export * from './YafSignatureTitle.js';
-import * as signatureTypes_1 from './signatureTypes/index.js';
-export { signatureTypes_1 as signatureTypes };
-//# sourceMappingURL=index.js.map
