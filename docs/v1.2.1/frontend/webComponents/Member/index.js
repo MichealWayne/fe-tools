f37f2ddbc3b66c06778c563f91330b9d5772d360
Wayne <michealwayne@163.com>
2026-06-16 13:50:55 +0000

    CI updated the documentation

---
+++ docs/v1.2.1/frontend/webComponents/Member/index.js
@@ -0,0 +1,8 @@
+export * from './YafMember.js';
+export * from './YafMemberDeclaration.js';
+export * from './YafMemberGetterSetter.js';
+export * from './YafMemberGroupLink.js';
+export * from './YafMemberGroupReflection.js';
+export * from './YafMemberSignatures.js';
+export * from './YafMemberSources.js';
+//# sourceMappingURL=index.js.map
