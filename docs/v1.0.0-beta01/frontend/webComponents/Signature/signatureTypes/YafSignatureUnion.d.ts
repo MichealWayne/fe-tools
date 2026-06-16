870c2558cc0c662e81481a88dda74dcda17b5031
Wayne <michealwayne@163.com>
2026-06-16 21:48:31 +0800

    feat update utils

--- docs/v1.0.0-beta01/frontend/webComponents/Signature/signatureTypes/YafSignatureUnion.d.ts
+++
@@ -1,5 +0,0 @@
-import { JSONOutput } from 'typedoc';
-import { YafHTMLElement } from '../../../index.js';
-export declare class YafSignatureUnion extends YafHTMLElement<JSONOutput.UnionType> {
-    onConnect(): void;
-}
